d3df42fd1f1246b75758c7f9403f7d3bf65d0eb6
Victor Jimenez <betabandido@gmail.com>
2014-12-07 22:03:41 -0500

    Added a unit test for load.data.file

--- tests/testthat/test-load-data.R
+++ tests/testthat/test-load-data.R
@@ -1,7 +1,7 @@
 library(stringr)
 context("Load data")
 
-test_that("Fields are correctly parsed", {
+test_that("Fields are correctly parsed in parse.fname", {
   expect_identical(
     parse.fname('test-1.csv', 'test-(\\d+)\\.csv', c('ID')),
     list('ID' = '1'))
@@ -27,4 +27,19 @@ test_that("Invalid parameters are detected in parse.fname", {
   expect_error(
     parse.fname('test.csv', 'test-(\\d+)\\.csv', c('ID')),
     'Pattern did not match file name')
+})
+
+test_that("load.data.file passes a minimal test", {
+  test.data <- data.table(X = 1:5, Y = (1:5) ^ 2)
+  fname <- tempfile(pattern = 'results-quad-', fileext = '.csv')
+  write.csv(test.data, fname, row.names = F)
+
+  dt <- load.data.file(fname, 'results-(\\w+)-.*\\.csv', c('METHOD'))
+  expect_true(all(colnames(dt) == c('X', 'Y', 'METHOD')))
+  expect_true(all(dt$METHOD == 'quad'))
+  expect_true(all(dt$Y == dt$X ^ 2))
+
+  dt <- load.data.file(fname, 'results-(\\w+)-.*\\.csv', c('METHOD'),
+                       function(dt) { dt[, Y := 2 * Y]})
+  expect_true(all(dt$Y == 2 * dt$X ^ 2))
 })
